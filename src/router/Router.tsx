fa6883c80a523a761af45ebcd6111de411c25175
six-standard <dbrrl1127@gmail.com>
2023-09-19 15:09:03 +0900

    🐛 :: Header, Footer 코드 위치 변경

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,24 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { styled } from "styled-components";
-import { Header } from "../components/Header";
 import { Main } from "../pages/Main";
-import { Footer } from "../components/Footer";
+import { Error } from "../pages/Error";
 
 export const Router = () => {
   return <BrowserRouter>
-    <Header />
     <Wrapper>
       <Routes>
+        <Route path="*" element={<Error/>} />
         <Route path="/" element={<Main />} />
       </Routes>
     </Wrapper>
-    <Footer />
   </BrowserRouter>
 }
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100vh;
   gap: 60px;
   display: flex;
   align-items: center;
