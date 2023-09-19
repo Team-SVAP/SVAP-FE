bfd316ef306157da5739df6bff2f73b14817c74c
six-standard <oddicat132@gmail.com>
2023-09-19 15:09:03 +0900

    🐛 :: Header, Footer 코드 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Router } from "./router/Router";
+import { GlobalStyle } from "./styles/style";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { styled } from "styled-components";
 
 export const App = () => {
   return <>
+    <GlobalStyle />
+    <Wrapper>
+      <Header />
+      <Router />
+      <Footer />
+    </Wrapper>
   </>
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
