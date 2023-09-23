8f3e4f2750dfdb7e972811ae67424ed4e6071240
six-standard <dbrrl1127@gmail.com>
2023-09-23 21:08:57 +0900

    🪓 :: src 폴더 삭제 병합 오류 해결용

--- src/styles/style.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-
-export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    user-select: none;
-    font-family: "IBM Plex Sans KR";
-  }
-`
