ebaeb466a3bb08e534eeccc7e2399bbfd652d7d0
six-standard <dbrrl1127@gmail.com>
2023-10-08 02:55:44 +0900

    🌧 :: style.ts 수정

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     border: none;
     outline: none;
+    background: none;
     user-select: none;
     text-decoration-line: none;
     font-family: "IBM Plex Sans KR";
