e3f2dbcc0de77f6fa965df4aee1708780ba5d254
six-standard <dbrrl1127@gmail.com>
2023-10-07 23:52:21 +0900

    ♻️ :: 코드 정리

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,28 @@
 import { styled } from 'styled-components';
 
-interface Props {
-  width: string;
-  height: string;
+interface IButton {
   text: string;
-  style?: object
-  clickAble: boolean;
+  style?: object;
+  disabled: boolean;
   action: () => void;
 }
 
-export const Button = ({ width, height, text, clickAble, style, action }: Props) => {
-  return <Wrapper width={width} height={height} clickAble={clickAble} onClick={action} style={style}>
+export const Button = ({ text, disabled, style, action }: IButton) => {
+  return <Component disabled={!disabled} onClick={action} style={style}>
     {text}
-  </Wrapper>
+  </Component>
 }
 
-const Wrapper = styled.div<{width:string, height:string, clickAble:boolean}>`
+const Component = styled.button<{disabled:boolean}>`
   display: flex;
   align-items: center;
   justify-content: center;
+  min-width: 10.25rem;
   transition: 0.2s all;
   border-radius: 1.25rem;
-  width: ${props => props.width};
-  height: ${props => props.height};
-  cursor: ${props => props.clickAble ? "pointer" : "not-allowed"};
-  background: var(${props => props.clickAble ? "--main700" : "--main500"});
+  padding: 10px 30px 10px 30px;
+  cursor: ${({disabled}) => !disabled ? "pointer" : "not-allowed"};
+  background: var(${({disabled}) => !disabled ? "--main700" : "--main500"});
   font-weight: 700;
   color: #ffffff;
   font-size: 1.25rem;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,14 @@
 import { styled } from 'styled-components';
 
-interface icon {
+interface Iicon {
   icon: string;
   action: () => void;
 }
 
-interface Props {
+interface IInput {
   type: string;
   value: string;
-  icon?: icon;
+  icon?: Iicon;
   width: string;
   height: string;
   id: string;
@@ -16,7 +16,7 @@ interface Props {
   change: (e: React.FormEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ type, value, icon, width, height, id, placeholder, change }: Props) => {
+export const Input = ({ type, value, icon, width, height, id, placeholder, change }: IInput) => {
   return <Wrapper htmlFor={id} width={width} height={height}>
     <_Input type={type} id={id} placeholder={placeholder} onChange={change} value={value} />
     { icon && <img src={icon.icon} alt="" onClick={icon.action} /> }
@@ -30,17 +30,16 @@ const Wrapper = styled.label<{width:string, height:string}>`
   border-radius: 1rem;
   transition: 0.2s all;
   box-sizing: border-box;
+  width: ${({width}) => width};
   padding: 0 1.25rem 0 1.25rem;
-  width: ${props => props.width};
-  height: ${props => props.height};
+  height: ${({height}) => height};
   border: 0.125rem solid var(--gray300);
-  &:focus-within { border: 0.125rem solid var(--main700 )}
+  &:focus-within { border: 0.125rem solid var(--main700)}
 `
 
 const _Input = styled.input`
   width: 80%;
   height: 1.25rem;
-  background: none;
   font-weight: 500;
   font-size: 1.063rem;
   &::placeholder { color: var(--gray300) }
