acb307aec64e3d3f0e931d802704f05134f3c8b6
six-standard <dbrrl1127@gmail.com>
2023-10-09 22:50:13 +0900

    🌧 :: 유저정보 API 추가

--- src/apis/Auth/index.tsx
+++ src/apis/Auth/index.tsx
@@ -2,13 +2,13 @@ import { IAuth, ISignUp } from "./Types";
 import { instance } from "../Axios";
 
 export const postLogin = async (Data: IAuth) => {
-  if(Data.accountId !== "" && Data.password !== "") {
-    return await instance.post("/user/login", Data);
-  };
+  return await instance.post("/user/login", Data);
 }
 
 export const postSignUp = async ({ Data, Admin }: ISignUp) => {
-  if(Data.userName !== "" && Data.accountId !== "" && Data.password !== "") {
-    return await instance.post(`/user/${Admin ? 'admin/signup' : 'signup'}`, Data);
-  };
+  return await instance.post(`/user/${Admin ? 'admin/signup' : 'signup'}`, Data);
+}
+
+export const getInfo = async () => {
+  return await instance.get("/user/my-info");
 }
