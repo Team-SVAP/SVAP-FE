5d01bfb3bc0052f5ca5c305635bcd42676e45690
six-standard <dbrrl1127@gmail.com>
2023-10-11 23:00:20 +0900

    🌧 :: 메인페이지 수정 인기글 불러오기 기능 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,8 +3,27 @@ import { Link } from 'react-router-dom';
 import { SearchBar } from '../components/SearchBar';
 import { imgPath } from '../utils/Paths';
 import '../styles/style';
+import { useState, useEffect } from 'react';
+import { getPopularPetition } from '../apis/Petition';
 
 export const Main = () => {
+  const [best, setBest] = useState({
+    title: "",
+    content: <></>,
+    id: ""
+  })
+
+  useEffect(() => {
+    getPopularPetition().then(res => {
+      console.log(res);
+      setBest({
+        title: res.data.title,
+        content: <>{(res.data.content).replaceAll("\n", <br />)}</>,
+        id: res.data.id
+      })
+    })
+  }, [])
+
   return <>
     <Slide src={`${imgPath.Sl}/1.png`} />
 
@@ -16,17 +35,11 @@ export const Main = () => {
     </Links>
 
     <Posts>
-      <PostsTitle><h1>인기 청원</h1><h2>더보기</h2></PostsTitle>
+      <PostsTitle><h1>인기 청원</h1><Link to="/search/best">더보기</Link></PostsTitle>
       <PostsData>
-        <h1>촉법소년 법 폐지 & 개정</h1>
-        <h2>요즘들어 나이 믿고 까불고 사고치고 사건 사고가 많은 나이 만 14이하 촉법소년들 이거 문제 심각합니다. 
-          빨리 법개정안하면 진짜 누구하나 죽을꺼임
-          이미 학폭으로 14세 미만 어린 피해자들이 많이 나오고도 있는 상황인대 무슨 근거와 깡으로 촉법소년 유지를 하고 있는건지 이유를 모르겠네요. 
-          14세 미만 아이들이 모여서 또래 친구들 폭행. 폭언. 성희롱. 성추행등으로 사망하는 피해자가 있고, 온갖 중범죄란 죄는 다저질르는대 촉법이란 이유로 형사처벌 면하고. 
-          촉법이 아니더라도 어리다.교화가 먼저고 교육이 먼저라는 이유로 형을 너무 낮게 한다던가.
-          뭐 이딴 개같은 법이 어딨고 뭐 있단 개같은 경우가 다있습니까? 마냑 피해자가 당신들 아들.딸이 ...
-        </h2>
-        <div>전체보기 <img src={`${imgPath.S}/Right.svg`} alt=""/></div>
+        <h1>{best.title}</h1>
+        <h2>{best.content}</h2>
+        <Link to={`/posts/${best.id}`}>전체보기 <img src={`${imgPath.S}/Right.svg`} alt=""/></Link>
       </PostsData>
     </Posts>
   </>
@@ -76,16 +89,18 @@ const LinkButton = styled(Link)`
 
 const PostsTitle = styled.div`
   display: flex;
+  align-items: center;
   justify-content: space-between;
   & > h1 { // 인기 청원
     font-weight: 600;
     font-size: 1.75rem;
     color: var(--gray600);
   }
-  & > h2 { // 더보기
+  & > a { // 더보기
     cursor: pointer;
     transition: 0.2s all;
-    font-size: 1.625rem;
+    font-size: 1.325rem;
+    font-weight: 600;
     color: var(--gray600);
     &:hover { color: var(--gray500); }
   }
@@ -105,7 +120,7 @@ const PostsData = styled.div`
     font-size: 1.5rem;
     user-select: text;
   }
-  & > div { // 전체보기 버튼
+  & > a { // 전체보기 버튼
     gap: 0.625rem;
     display: flex;
     align-items: center;
