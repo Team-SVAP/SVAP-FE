cec5129b23c2baea306b3293a6b2a655fdf73015
six-standard <dbrrl1127@gmail.com>
2023-10-19 23:07:21 +0900

    ♻️ :: 에러 페이지 리팩토링 파일 경로 수정 및 파일 분할

--- src/pages/Error/Style.ts
+++ src/pages/Error/Style.ts
@@ -1,15 +1,7 @@
 import { styled } from 'styled-components';
-import '../styles/color.css';
 
-export const Error = () => {
-  return <Wrapper>
-    <h1>404</h1>
-    <Line />
-    <h2>페이지를 찾을 수 없습니다</h2>
-  </Wrapper>
-}
-
-const Wrapper = styled.div`
+// Box Components
+export const Wrapper = styled.div`
   gap: 1.25rem;
   display: flex;
   align-items: center;
@@ -26,8 +18,9 @@ const Wrapper = styled.div`
   }
 `
 
-const Line = styled.div`
+// Atom Components
+export const Line = styled.div`
   width: 0.125rem;
   height: 6.25rem;
   background: var(--gray600);
-`
+  `
---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,9 @@
+import * as _ from './Style';
+
+export const Error = () => {
+  return <_.Wrapper>
+    <h1>404</h1>
+    <_.Line />
+    <h2>페이지를 찾을 수 없습니다</h2>
+  </_.Wrapper>
+}
