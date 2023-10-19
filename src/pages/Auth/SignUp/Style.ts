829548dd449049a4dfed1a355ddc70a4b39cd3f4
six-standard <dbrrl1127@gmail.com>
2023-10-20 08:53:10 +0900

    ♻️ :: 회원가입 스타일 경로 변경

---
+++ src/pages/Auth/SignUp/Style.ts
@@ -0,0 +1,17 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  gap: 5vh;
+  display: flex;
+  place-self: start;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`
+
+export const Page = styled.h1`
+  align-self: flex-end;
+  font-size: 30px;
+  font-weight: 500px;
+  color: var(--gray600);
+`
