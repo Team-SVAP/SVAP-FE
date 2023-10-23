ea3f45413676d0d5f75fb57ceea27d183cec6a21
six-standard <dbrrl1127@gmail.com>
2023-10-23 22:29:19 +0900

    ♻️ :: 쓸모없는 컴포넌트 제거

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -8,10 +8,6 @@ export const Input = ({ type, value, icon, width, height, id, placeholder, chang
   </Component>
 }
 
-export const TextArea = ({ value, width, height, id, placeholder, change }: ITextArea) => {
-  return <_Textarea value={value} width={width} height={height} id={id} placeholder={placeholder} onChange={change} />
-}
-
 const Component = styled.label<{width:string, height:string}>`
   display: flex;
   align-items: center;
@@ -23,9 +19,4 @@ const Component = styled.label<{width:string, height:string}>`
 const _Input = styled.input`
   width: 100%;
   height: 1.25rem;
-`
-
-const _Textarea = styled.textarea<{width: string, height: string}>`
-  width: ${({width}) => width};
-  height: ${({height}) => height};
 `
