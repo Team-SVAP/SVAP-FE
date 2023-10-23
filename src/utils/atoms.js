3e3b047493afb8cebe05f72ce536c51d49843955
six-standard <dbrrl1127@gmail.com>
2023-10-23 22:29:06 +0900

    ♻️ :: 쓸모없는 주석 제거

--- src/utils/atoms.js
+++ src/utils/atoms.js
@@ -1,10 +1,5 @@
 import { atom } from 'recoil';
 
-// export const searchData = atom({
-//   key: 'searchData',
-//   default: ''
-// })
-
 export const Modal = atom({
   key: 'Modal',
   default: {
