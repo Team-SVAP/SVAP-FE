6e97815041e5cb0e100766d036548b90cc8359e0
six-standard <dbrrl1127@gmail.com>
2023-10-25 20:24:18 +0900

    🌧 :: 모달 스타일 업데이트 이미지 스타일 추가

--- src/styles/modalStyle.ts
+++ src/styles/modalStyle.ts
@@ -17,3 +17,22 @@ export const AreaInput = styled.textarea`
   width: 100%;
   height: 16.875rem;
 `
+
+export const Image = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: space-between;
+  & > img#image {
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 70vh;
+    transition: none;
+    box-sizing: border-box;
+    border-radius: 0.938rem;
+    border: 0.125rem solid whitesmoke;
+    box-shadow: 0.125rem 0.125rem 0.625rem whitesmoke;
+  }
+`
